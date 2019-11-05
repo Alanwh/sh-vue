b3e69083e103571709347ac0dac2a3f222f964aa
hewenjun-jk <hewenjun-jk@360jinrong.net>
2019-11-05 16:58:33 +0800

    perf: 优化htmls

--- conf/htmls.js
+++ conf/htmls.js
@@ -1,12 +1,13 @@
 const constant = require('./constant');
 
-module.exports = (PROJECT) => {
+module.exports = (app) => {
   return {
-    PROJECT: {
-      entry: `${PROJECT}-vue/main.js`,
+    app: {
+      // js 下生成文件名已app命名，这里用PROJECT变量不生效，原因未知
+      entry: `${app}-vue/main.js`,
       template: 'public/index.html',
       filename: 'index.html',
-      title: constant.PAGE_TITLE[PROJECT]
+      title: constant.PAGE_TITLE[app]
     }
   }
 }
